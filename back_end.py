189c67e167e8788dd2cfb0ba9893e2402c4eb52e
Mr.왕박살 <taeeonkm@gmail.com>
2024-11-13 20:14:02 -0300

    up back end!
    starting with back end!!!!!!

---
+++ back_end.py
@@ -0,0 +1,21 @@
+
+
+class Conversor:
+
+    def __init__(self):
+        
+        self.keys = {"ADD":"0001" , "AND":"0101" ,"BR":"0000" , "JMP":"1100", "JSR":"0100" ,
+                     "LD":"0010", "LDI":"1010", "LDR":"0110" , "LEA":"1110" , "NOT":"1001" ,
+                     "RET":"1100" , "RTI":"1000" , "ST":"0011" , "STI":"1011" ,"STR":"0111" ,
+                     "TRAP":"1111" , "reserved":"1101"}
+        
+        self.keysSp = {}
+
+
+    def assambly_to_binary (self,textAssambly:str):
+
+        pass
+
+    def binary_to_assambly (self,textBinary:str):
+
+        pass
